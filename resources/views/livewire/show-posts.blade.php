0db9e64da2fe61ab5832291373fedeb9f4106457
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2024-08-02 08:08:10 -0600

    Creando entidad Posts y mostrando data en el componente de livewire

---
+++ resources/views/livewire/show-posts.blade.php
@@ -0,0 +1,12 @@
+<div>
+    {{-- The best athlete wants his opponent at his best. --}}
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <h1>Hola mundo - Livewire</h1>
+
+    {{ $posts }}
+</div>
